c3154e61b69232f1dc8d2c384b1bc950623cb90f
Gabriel de Oliveira Ferreira <38558405+gabrielferreirah@users.noreply.github.com>
2018-05-21 11:27:13 -0300

    criando a interface do produto
    ainda precisa declarar o metodo pra ser funcional

--- Sistema.java
+++ Sistema.java
@@ -5,10 +5,24 @@
  */
 package SistemaPadaria;
 
+import javax.swing.JOptionPane;
+
 /**
  *
  * @author Gabriel de Oliveira Ferreira
  */
 public class Sistema {
+
+    int menu = Integer.parseInt(JOptionPane.showInputDialog(null,
+            "1 - Cliente"
+            + "\n2 - Produtos"
+            + "\n3 - Funcionário"
+            + "\n4 - Entregas"
+            + "\n5 - Sair"));
     
+    while(menu != 5){
+        switch(menu){
+            
+        }
+}
 }
