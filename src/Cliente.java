fca25e8c403305d47bbf509cd4c8de7fdc439b4c
Carlosaugustob <38558653+Carlosaugustob@users.noreply.github.com>
2018-05-21 11:26:26 -0300

    CLIENTE
    Criando as informações básicas do cliente.

---
+++ src/Cliente.java
@@ -0,0 +1,18 @@
+
+import javax.swing.JOptionPane;
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author Alunos Carlos Augusto Borged e Gabriel de Oliveira Ferreira. 
+ */
+public class Cliente {
+    
+String nome = JOptionPane.showInputDialog("Digite o seu nome ");
+
+        }
