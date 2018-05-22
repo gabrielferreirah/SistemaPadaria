df4de80df6278a2cd9a69e389ed3a0b0804935e9
Gabriel de Oliveira Ferreira <38558405+gabrielferreirah@users.noreply.github.com>
2018-05-22 09:07:23 -0300

    CEDUP - 22/05 -09:05
    Inicialização do menu dos produtos

---
+++ src/Produtos.java
@@ -0,0 +1,24 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author Alunos
+ */
+public class Produtos {
+    
+    String[] nomes = new String[100];
+    String[] ingredientes = new String[100];
+    String[] receitas = new String[100];
+    int[] valor = new int[100];
+    String[] validadeDoProduto = new String[100];
+    double[] lucro = new double[100];
+    String[] valorCalorico = new String[100];
+    
+    public void buscarPeloNome(){
+        
+    }
+}
