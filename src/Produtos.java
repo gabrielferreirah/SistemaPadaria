4c6e7baf208d8a4558b2f18b08730ce3059926ed
Gabriel de Oliveira Ferreira <38558405+gabrielferreirah@users.noreply.github.com>
2018-05-24 08:17:58 -0300

    CEDUP - 24/05 - 08:15
    Pequenas mudanças nas funcionalidades do menu produtos.

--- src/Produtos.java
+++ src/Produtos.java
@@ -6,13 +6,12 @@ import javax.swing.JOptionPane;
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-
 /**
  *
- * @author Gabriel de Oliveira Ferreira
+ * @author Gabriel de Oliveira Ferreira e Carlos Augusto Borges
  */
 public class Produtos {
-    
+
     String[] nomes = new String[100];
     String[] ingredientes = new String[100];
     String[] receitas = new String[100];
@@ -21,26 +20,50 @@ public class Produtos {
     double[] lucro = new double[100];
     String[] valorCalorico = new String[100];
     String[] alergicos = new String[100];
-    
-    public void buscarPeloNome(){
-        //TODO implementar a busca pelo nome
+    int atual = 0;
+
+    public void buscarPeloNome() {
+        String busca = JOptionPane.showInputDialog("Digite o nome parcial para a busca");
+
+        for (int i = 0; i < atual; i++) {
+            if (nomes[i].contains(busca)) {
+                apresentarInformacao(i);
+            }
+        }
     }
-    
-    public void buscarPeloValorCalorico(){
+
+    public void buscarPeloValorCalorico() {
         //TODO implementar a busca pelo valor calorico
     }
-    
-    public void solicitarInformacao(int posicao){
+
+    public void solicitarInformacao(int posicao) {
         nomes[posicao] = JOptionPane.showInputDialog("Digite o seu nome");
         valores[posicao] = Double.parseDouble(JOptionPane.showInputDialog("Digite o valor do produto"));
         alergicos[posicao] = JOptionPane.showInputDialog("Digite os componentes alergicos do produto");
     }
-    
-    public void apresentarInformacao(){
-        //TODO apresentar as informações
+
+    public void apresentarInformacao(int posicao) {
+        JOptionPane.showMessageDialog(null,
+                "Produto: " + nomes[posicao]
+                + "Valor: " + valores[posicao]
+                + "Validade: " + validadeDoProduto[posicao]
+        );
     }
-    
+
     public void apresentarReceita() {
         //TODO apresentar a receita
     }
+
+    public void cadastrarProdutos() {
+        //TODO implenetar cadastro de produtos   
+    }
+
+    public void editar() {
+        //TODO implementar edição das informacões
+    }
+    
+    public void buscarPeloIngrediente() {
+        
+    }
+
 }
